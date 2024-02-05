523fc2c128a15267977e671da4ffb768e10d52a9
Aashnaa <missaashnaa1@gmail.com>
2024-02-05 15:16:42 +0530

    route: added /test

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return "Hello World!"
+
+@app.route('/test')
+def test():
+  return "Test!"
 
 @app.route('/dojo')
 def dojo():
